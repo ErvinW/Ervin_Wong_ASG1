747cdbae2956252849ef059b726dd50d5ec4f891
ErvinW <116048866+ErvinW@users.noreply.github.com>
2022-11-28 13:44:39 +0800

    function
    started building function

---
+++ script.js
@@ -0,0 +1,13 @@
+function AudioItems(name, build, app, anc, batt, ipx,
+    con, desc, purhclink, imglink){
+        this.name = name;
+        this.build = build;
+        this.app = app;
+        this.anc = anc;
+        this.batt = batt;
+        this.ipx = ipx;
+        this.con = con;
+        this.desc = desc;
+        this.purhclink = purhclink;
+        this.imglink = imglink;
+    }
